db98227d236c3428fca93d03e3c1c4df5029bea9
Anish Gowda <anishgowda43@gmail.com>
2020-11-08 17:03:05 +0530

    first
    Signed-off-by: Anish Gowda <anishgowda43@gmail.com>

---
+++ code/javaforlogin.js
@@ -0,0 +1,21 @@
+// JavaScript Document]
+
+function validate(){
+	var x=document.getElementById("user");
+
+var y=document.getElementById("pass");
+
+
+
+if(x.value!="dscrit@gmail.com" || y.value!="dscrit"){
+	alert("User Name Or Password 'ERROR'");
+	return false;
+	
+	
+}
+	else
+		{
+			true;
+			
+		}
+}
